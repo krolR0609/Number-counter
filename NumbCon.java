e21249906484ad10d389cd531f2347b1978c9e78
Krolr0609 <nick.arty@gmail.com>
2016-10-09 19:36:41 +0300

    1.0.0

--- NumbCon.java
+++ NumbCon.java
@@ -1,113 +1,116 @@
-import java.util.Scanner;
 public class NumbCon{
+	static String result = "";
 	public static void getLast(int numb){
-		switch (numb){
-			case 1: System.out.println("Один");
+		switch ((numb%100)%10){
+			case 1: result += "Один";
 			break;
-			case 2: System.out.println("Два");
+			case 2: result += "Два"; 
 			break;
-			case 3: System.out.println("Три");
+			case 3: result += "Три";
 			break;
-			case 4: System.out.println("Четыре");
+			case 4: result += "Четыре";
 			break;
-			case 5: System.out.println("Пять");
+			case 5: result += "Пять";
 			break;
-			case 6: System.out.println("Шесть");
+			case 6: result += "Шесть";
 			break;
-			case 7: System.out.println("Семь");
+			case 7: result += "Семь";
 			break;
-			case 8: System.out.println("Восемь");
+			case 8: result += "Восемь";
 			break;
-			case 9: System.out.println("Девять");
+			case 9: result += "Девять";
 			break;
 		}
+		
 	}
 	public static void getSecond(int numb){
-		switch (numb){
+		switch ((numb%100)/10){
+			case 0: getLast(numb);
+			break;
 			case 1: getElse(numb%10);
 			break;
-			case 2: System.out.println("Двадцать");
+			case 2: result += "Двадцать ";
 			break;
-			case 3: System.out.println("Тридцать");
+			case 3: result += "Тридцать ";
 			break;
-			case 4: System.out.println("Сорок");
+			case 4: result += "Сорок ";
 			break;
-			case 5: System.out.println("Пятьдесят");
+			case 5: result += "Пятьдесят ";
 			break;
-			case 6: System.out.println("Шестьдесят");
+			case 6: result += "Шестьдесят ";
 			break;
-			case 7: System.out.println("Семьдесят");
+			case 7: result += "Семьдесять ";
 			break;
-			case 8: System.out.println("Восемьдесят");
+			case 8: result += "Восемьдесять ";
 			break;
-			case 9: System.out.println("Девяносто");
+			case 9: result += "Девяносто ";
 			break;
 		}
 	}
 	public static void getFirst(int numb){
-		switch (numb){
-			case 1: System.out.println("Сто");
+		switch (numb/100){
+			case 1: result += "Сто ";
 			break;
-			case 2: System.out.println("Двести");
+			case 2: result += "Двести ";
 			break;
-			case 3: System.out.println("Триста");
+			case 3: result += "Триста ";
 			break;
-			case 4: System.out.println("Четыреста");
+			case 4: result += "Четыреста ";
 			break;
-			case 5: System.out.println("Пятьсот");
+			case 5: result += "Пятьсот ";
 			break;
-			case 6: System.out.println("Шестьсот");
+			case 6: result += "Шестьсот ";
 			break;
-			case 7: System.out.println("Семьсот");
+			case 7: result += "Семьсот ";
 			break;
-			case 8: System.out.println("Восемьсот");
+			case 8: result += "Восемьсот ";
 			break;
-			case 9: System.out.println("Девятьсот");
+			case 9: result += "Девятьсот ";
 			break;
 		}
 	}
-		public static void getElse(int numb){
-		switch (numb){
-			case 0: System.out.println("Десять");
+	public static void getElse(int numb){
+		switch (numb%10){
+			case 0: result += "Десять ";
 			break;
-			case 1: System.out.println("Одиннадцать");
+			case 1: result += "Одиннадцать ";
 			break;
-			case 2: System.out.println("Двеннадцать");
+			case 2: result += "Двенадцать ";
 			break;
-			case 3: System.out.println("Тринадцать");
+			case 3: result += "Тринадцать ";
 			break;
-			case 4: System.out.println("Четырнадцать");
+			case 4: result += "Четырнадцать ";
 			break;
-			case 5: System.out.println("Пятьнадцать");
+			case 5: result += "Пятнадцать ";
 			break;
-			case 6: System.out.println("Шестьнадцать");
+			case 6: result += "Шестнадцать ";
 			break;
-			case 7: System.out.println("Семьнадцать");
+			case 7: result += "Семнадцать ";
 			break;
-			case 8: System.out.println("Восемьнадцать");
+			case 8: result += "Восемнадцать ";
 			break;
-			case 9: System.out.println("Девятьнадцать");
+			case 9: result += "Девятнадцать ";
 			break;
 		}
 	}
 	public static void main(String[] args) {
 		int numb = 0;
 		if(args.length > 0){
-			numb = Integer.parseInt(args[0]);  
-		}
-		if(numb>=100){
-			getFirst(numb/100);
-			getSecond((numb/10)%10);
-			getLast(numb%10);
-		}else{
-			if(numb>=10 ){
-				getSecond(numb/10);
-				getLast(numb%10);
-			}else{
-				getLast(numb%10);
+			numb = Integer.parseInt(args[0]); 
+			if(numb > 999 || numb < 1){
+				System.out.println("Out of range");
+			}else {
+				getFirst(numb);
+				getSecond(numb);
+				if((numb%100) > 19){
+					getLast(numb);
+				}
 			}
+			System.out.println("Вы ввели - " + numb);
+			System.out.println(result);
+		}else {
+			System.out.println("Enter the data\nlike: java NumbCon 12");
+			System.out.println("===========================================\n Compile with -encoding utf8\n===========================================");
 		}
-		
-    	//System.out.println(String.valueOf(Math.abs(numb)).length());
 	}
 }
